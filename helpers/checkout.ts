ff3fc35746ceb62f2ea28b1c161492460e6bf2d9
Isabel Galdamez <isabelgaldamez@sbcglobal.net>
2026-05-20 11:59:53 -0700

    clean checkout tests

--- helpers/checkout.ts
+++ helpers/checkout.ts
@@ -1,25 +1,5 @@
 import {type Page } from '@playwright/test';
 import { CheckoutPage } from '../locators/checkout';
-// export class CheckoutHelper {
-//     readonly page : Page;
-//     // const checkoutPage = new CheckoutPage(page);
-
-//     constructor (page : Page){
-//         this.page = page;
-//         const checkoutPage = new CheckoutPage(page);
-
-//         async function addItemToCart(page){
-//             const productName = await checkoutPage.productName.innerText()
-//             const productPrice = await page.locator('[data-test-id="product-card-504"] span').innerText()
-//             await page.click("[data-test-id='product-card-add-to-cart-button-504']");
-        
-//             return {
-//                 productName,
-//                 productPrice
-//             }
-//     }
-// }
-
 
 export async function addItemToCart(page : Page){
     const checkoutPage = new CheckoutPage(page);
@@ -32,8 +12,3 @@ export async function addItemToCart(page : Page){
         productPrice
     }
 }
-
-// export async function itemsQtyAddedToCart(page : Page){
-//     const itemsQtyAddedToCart = await 
-
-// }
