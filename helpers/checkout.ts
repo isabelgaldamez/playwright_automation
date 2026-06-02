f988f8824a4f833b55b4f7244a269d2f04180616
Isabel Galdamez <isabelgaldamez@sbcglobal.net>
2026-06-02 12:14:56 -0700

    track order test

--- helpers/checkout.ts
+++ helpers/checkout.ts
@@ -12,3 +12,51 @@ export async function addItemToCart(page : Page){
         productPrice
     }
 }
+export async function proceedToCheckout(page: Page){
+    const checkoutPage = new CheckoutPage(page);
+    checkoutPage.proceedToCheckoutBtn.click();
+}
+export async function fillOutContactInformation(page: Page, firstName: string, lastName: string, email: string){
+    const checkoutPage = new CheckoutPage(page);
+    await checkoutPage.firstNameInput.fill(firstName);
+    await checkoutPage.lastNameInput.fill(lastName);
+    await checkoutPage.emailInput.fill(email);
+}
+export async function fillOutAddressInformation(page: Page, address: string, city: string, zipCode: string){
+    const checkoutPage = new CheckoutPage(page);
+    await checkoutPage.addressInput.fill(address);
+    await checkoutPage.cityInput.fill(city);
+    await checkoutPage.zipCodeInput.fill(zipCode);
+}
+export async function fillOutCardInformation(page: Page, nameOnCard : string,  cardNumber : string, expDate: string, cvc : string){
+    const checkoutPage = new CheckoutPage(page);
+    await checkoutPage.nameOnCardInput.fill(nameOnCard);
+    await checkoutPage.cardNumberInput.fill(cardNumber);
+    await checkoutPage.expirationDateInput.fill(expDate);
+    await checkoutPage.cvcInput.fill(cvc);
+}
+export async function placeOrderBtn(page: Page){
+    const checkoutPage = new CheckoutPage(page);
+    await checkoutPage.placeOrderBtn.click();
+}
+export async function verifyOrderConfirmation(page: Page){
+    const checkoutPage = new CheckoutPage(page)
+    const orderConfirmationHeader = await checkoutPage.orderConfirmationHeader.innerText();
+    const orderConfirmationMessage = await checkoutPage.orderConfirmationMessage.innerText();
+    return {
+        orderConfirmationHeader,
+        orderConfirmationMessage
+
+    }
+}
+
+export async function getOrderId(page : Page){
+    const checkoutPage = new CheckoutPage(page)
+    const orderId = await checkoutPage.orderId.innerText();
+    return orderId;
+}
+
+export async function trackOrder(page: Page){
+    const checkoutPage = new CheckoutPage(page);
+    await checkoutPage.trackOrderLink.click();
+}
---
+++ locators/tracking.ts
@@ -0,0 +1,32 @@
+import {Selectors, type Locator, type Page } from '@playwright/test'
+export class TrackingPage {
+    readonly page: Page;
+    readonly trackOrderHeader : Locator;
+    readonly orderIdInput : Locator;
+    readonly emailAddressInput : Locator;
+    readonly trackOrderBtn : Locator
+
+    constructor(page: Page) {
+        this.page = page;
+        this.trackOrderHeader = page.locator('div.grid > div:nth-child(2) h3.tracking-tight:nth-child(1)')
+        this.orderIdInput = page.locator("[data-test-id='contact-order-id-input']")
+        this.emailAddressInput = page.locator("[data-test-id='contact-email-input']")
+        this.trackOrderBtn = page.locator("[data-test-id='contact-track-order-button']")  
+    }
+    async orderDetailHeader(trackingId : string){
+        return this.page.locator(`[data-test-id='order-detail-page-${trackingId}'] h3:nth-child(1)`)
+    }
+    async orderedItem(trackingId : string){
+        return this.page.locator(`[data-test-id='order-detail-page-${trackingId}'] div.justify-between p:nth-child(1)`)
+    }
+    async itemPrice(trackingId : string ){
+        return this.page.locator(`[data-test-id='order-detail-page-${trackingId}'] div.justify-between > p`)
+    }
+    async customerName(trackingId: string){
+        return this.page.locator(`[data-test-id='order-detail-page-${trackingId}'] h3:nth-child(4) + div > p:nth-child(1)`)
+    }
+    async customerEmail(trackingId: string){
+        return this.page.locator(`[data-test-id='order-detail-page-${trackingId}'] h3:nth-child(4) + div > p:nth-child(2)`)
+    }
+
+}
