457779124ac813515a3c996c1f711e5f42111861
Isabel Galdamez <isabelgaldamez@sbcglobal.net>
2026-06-10 13:13:19 -0700

    modified getByText tests

--- tests/getByText.spec.ts
+++ tests/getByText.spec.ts
@@ -9,13 +9,13 @@ describe('GetByText - Use getByText locators', async (page) => {
     });
     // GetByText can be used for dynamic text in the application.
     test('GetByText - As a visitor, I want to read the heading text of Registration page', async ({page}) => {
-        const titleText = page.getByText('Create your account');
-        expect(titleText).toBeVisible({timeout: 3000});
+        const titleText = await page.getByText('Create your account');
+        expect(titleText).toBeVisible({timeout: 5000});
     });
     test('GetByText, validate error message at username input',async ({page}) => {
         await page.click('[data-testid="register-btn"]')
-        const emailErrorMessage = page.getByText('Enter a valid email');
-        const passwordErrorMessage = page.getByText('Password does not meet the requirements below');
+        const emailErrorMessage = await page.getByText('Enter a valid email');
+        const passwordErrorMessage = await page.getByText('Password does not meet the requirements below');
         expect(emailErrorMessage).toBeVisible();
         expect(passwordErrorMessage).toBeVisible();
     });
@@ -25,7 +25,7 @@ describe('GetByText - Use getByText locators', async (page) => {
         await emailInput.fill('username@test.com')
         const passwordInput = await page.getByTestId('register-password');
         await passwordInput.fill('Test');
-        const confirmPasswordInput = page.getByRole('textbox', { name: 'Password'});
+        const confirmPasswordInput = await page.getByRole('textbox', { name: 'Password'});
         await confirmPasswordInput.fill('Test');
         await page.click('[data-testid="register-btn"]')
         const passwordErrorMessage = await page.getByText('Password does not meet the requirements below');
